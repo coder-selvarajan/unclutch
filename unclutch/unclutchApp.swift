c20c77711952e6ed827b90a8faac73cb9f13e6bb
Selvarajan Thangavel <selvarajan2006@gmail.com>
2024-12-23 17:27:51 +0530

    Initial Commit

---
+++ unclutch/unclutchApp.swift
@@ -0,0 +1,17 @@
+//
+//  unclutchApp.swift
+//  unclutch
+//
+//  Created by Selvarajan on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct unclutchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
